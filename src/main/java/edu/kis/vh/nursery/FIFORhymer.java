cb6ea4ec53a7aba4dd6f91c7739db3776abc07b6
Bartłomiej Nowak <bartekiszymek1@gmail.com>
2024-06-20 11:40:41 +0200

    17. dodanie dokumentacji do klasy FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,16 +1,29 @@
 package edu.kis.vh.nursery;
 
+/**
+ * FIFORhymer is a specialized type of DefaultCountingOutRhymer.
+ * It implements a FIFO (First-In-First-Out) behavior for the rhymer.
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
+    // Temporary rhymer used for implementing FIFO behavior
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+    /**
+     * Overrides the countOut method to provide FIFO behavior.
+     *
+     * @return The value of the first item that was added (FIFO).
+     */
     @Override
     protected int countOut() {
+        // Move all elements from the main rhymer to the temp rhymer
         while (!callCheck())
             temp.countIn(super.countOut());
 
+        // Retrieve the first element added (which is now on the top of the temp rhymer)
         int ret = temp.countOut();
 
+        // Move all elements back from the temp rhymer to the main rhymer
         while (!temp.callCheck())
             countIn(temp.countOut());
 
